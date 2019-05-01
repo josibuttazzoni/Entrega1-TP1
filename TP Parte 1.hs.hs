59f891c8f17ebeea3a7c70615f126afe83fa632d
josibuttazzoni <48684533+josibuttazzoni@users.noreply.github.com>
2019-05-01 17:19:43 -0300

    Update TP Parte 1.hs.hs

--- TP Parte 1.hs.hs
+++ TP Parte 1.hs.hs
@@ -1,16 +1,22 @@
 import Text.Show.Functions
-data Auto = Auto {nombre :: String, nivelNafta :: Int, velocidad :: Int , nombreEnamorade :: String, truco :: Auto -> Auto} deriving Show
+data Auto = Auto {nombre :: String, nivelNafta :: Int, velocidad :: Int , nombreEnamorade :: String, truco :: Truco} deriving (Show)
 type Truco = Auto -> Auto
 
 data Carrera = Carrera {nombreCarrera :: String, largoPista :: Float, cantidadVueltas :: Int, publico :: [String], participantes :: [Auto], trampa :: Trampa} deriving (Show)
 type Trampa = Carrera -> Carrera
 
+
+rochaMcQueen = Auto "RochaMcQueen" 300 0 "Ronco" deReversa
+biankerr = Auto "Biankerr" 500 20 "Tinch" impresionar
+gusthav = Auto "Gusthav" 200 130 "PetiLaLinda" nitro
+rodra = Auto "Rodra" 0 50 "Taisa" (fingirAmor ("Petra"))
+
 potreroFunes = Carrera "potreroFunes" 5.0 3 ["Ronco", "Tinch", "Dodain"] [rochaMcQueen, biankerr, gusthav, rodra] sacarAlPistero
+
+cambiarVelocidad unAuto incremento = unAuto {velocidad=incremento.velocidad $unAuto}
 
 deReversa :: Truco
 deReversa unAuto = unAuto {nivelNafta = div (velocidad unAuto) 5 + nivelNafta unAuto}
-
-cambiarVelocidad unAuto incremento = unAuto {velocidad=incremento.velocidad $unAuto}
 
 impresionar :: Truco
 impresionar unAuto = cambiarVelocidad unAuto (*2)
@@ -19,12 +25,7 @@ nitro :: Truco
 nitro unAuto = cambiarVelocidad unAuto (+15)
 
 fingirAmor :: String -> Truco
-fingirAmor unAuto enamoradeConveniencia = unAuto {nombreEnamorade = enamoradeConveniencia}
-
-rochaMcQueen = Auto "RochaMcQueen" 300 0 "Ronco" deReversa
-biankerr = Auto "Biankerr" 500 20 "Tinch" impresionar
-gusthav = Auto "Gusthav" 200 130 "PetiLaLinda" nitro
-rodra = Auto "Rodra" 0 50 "Taisa" (fingirAmor ("Petra"))
+fingirAmor enamoradeConveniencia unAuto = unAuto {nombreEnamorade = enamoradeConveniencia}
 
 cantidadVocales :: Auto -> Int
 cantidadVocales unAuto = length.filter (\letra -> elem letra "aeiouAEIOU").nombreEnamorade $unAuto
