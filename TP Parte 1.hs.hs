978fb9762f87acf87e87fa33073dd1d858e3ac5e
josibuttazzoni <48684533+josibuttazzoni@users.noreply.github.com>
2019-05-04 18:13:29 -0300

    Modifico realizanSuTruco (hecha con guardas)

--- TP Parte 1.hs.hs
+++ TP Parte 1.hs.hs
@@ -1,8 +1,8 @@
 import Text.Show.Functions
-data Auto = Auto {nombre :: String, nivelNafta :: Int, velocidad :: Int , nombreEnamorade :: String, truco :: Truco} deriving (Show)
+data Auto = Auto {nombre :: String, nivelNafta :: Float, velocidad :: Float , nombreEnamorade :: String, truco :: Truco} deriving (Show)
 type Truco = Auto -> Auto
 
-data Carrera = Carrera {nombreCarrera :: String, largoPista :: Int, cantidadVueltas :: Int, publico :: [String], participantes :: [Auto], trampa :: Trampa} deriving (Show)
+data Carrera = Carrera {nombreCarrera :: String, largoPista :: Float, cantidadVueltas :: Int, publico :: [String], participantes :: [Auto], trampa :: Trampa} deriving (Show)
 type Trampa = Carrera -> Carrera
 
 rochaMcQueen = Auto "RochaMcQueen" 300 0 "Ronco" deReversa
@@ -15,7 +15,7 @@ potreroFunes = Carrera "potreroFunes" 5 3 ["Ronco", "Tinch", "Dodain"] [rochaMcQ
 cambiarVelocidad unAuto incremento = unAuto {velocidad=incremento.velocidad $unAuto}
 
 deReversa :: Truco
-deReversa unAuto = unAuto {nivelNafta = div (velocidad unAuto) 5 + nivelNafta unAuto}
+deReversa unAuto = unAuto {nivelNafta = velocidad unAuto / 5 + nivelNafta unAuto}
 
 impresionar :: Truco
 impresionar unAuto = cambiarVelocidad unAuto (*2)
@@ -44,9 +44,16 @@ comboLoco = deReversa.nitro
 queTrucazo :: String -> Truco
 queTrucazo enamoradeConveniencia = incrementarVelocidad.fingirAmor enamoradeConveniencia
 
-turbo :: Truco
-turbo unAuto = unAuto {velocidad = ((+velocidad unAuto).(*10).nivelNafta) unAuto , nivelNafta = 0} 
+vaciarNafta :: Truco
+vaciarNafta unAuto = unAuto {nivelNafta=0}
 
+velocidadTurbo :: Truco
+velocidadTurbo unAuto = cambiarVelocidad unAuto (+(10 * nivelNafta unAuto))
+
+turbo :: Truco
+turbo = vaciarNafta.velocidadTurbo
+ 
+trampaBase :: Carrera -> ([Auto]->[Auto]) -> Carrera
 trampaBase unaCarrera funcion = unaCarrera {participantes = funcion.participantes $unaCarrera}
 
 sacarAlPistero :: Trampa
@@ -74,7 +81,7 @@ podio :: Trampa
 podio unaCarrera = trampaBase unaCarrera (take 3)
 
 restarNafta :: Carrera -> Truco
-restarNafta unaCarrera unAuto = unAuto {nivelNafta = nivelNafta unAuto - (div (largoPista unaCarrera) 10) * (velocidad unAuto)}
+restarNafta unaCarrera unAuto = unAuto {nivelNafta = nivelNafta unAuto - (largoPista unaCarrera / 10) * (velocidad unAuto)}
 
 restarNaftaParticipantes :: [Auto] -> Carrera -> Carrera
 restarNaftaParticipantes autos unaCarrera = unaCarrera {participantes = map(restarNafta unaCarrera) autos}
@@ -85,15 +92,19 @@ suEnamoradeEstaEnElPublico unaCarrera unAuto = elem (nombreEnamorade unAuto) (pu
 realizarTruco :: Auto -> Auto
 realizarTruco unAuto = truco unAuto unAuto
 
-realizaSuTruco :: Trampa
-realizaSuTruco unaCarrera = unaCarrera {participantes = (map(realizarTruco).filter(suEnamoradeEstaEnElPublico unaCarrera).participantes $unaCarrera) 
-++ (filter(not.suEnamoradeEstaEnElPublico unaCarrera).participantes $unaCarrera)}
+realizanSuTruco :: Carrera -> Auto -> Auto
+realizanSuTruco unaCarrera unAuto
+    |suEnamoradeEstaEnElPublico unaCarrera unAuto == False = unAuto
+    |otherwise = realizarTruco unAuto
+
+mapeoTruco :: Carrera -> Carrera
+mapeoTruco unaCarrera = unaCarrera {participantes=map(realizanSuTruco unaCarrera).participantes $unaCarrera}
 
 darVuelta :: Carrera -> Carrera
-darVuelta unaCarrera = trampa(unaCarrera).realizaSuTruco.restarNaftaParticipantes(participantes unaCarrera) $unaCarrera
+darVuelta unaCarrera = trampa(unaCarrera).mapeoTruco.restarNaftaParticipantes(participantes unaCarrera) $unaCarrera
 
 correrCarrera :: Carrera -> Carrera
 correrCarrera unaCarrera = iterate darVuelta unaCarrera !! (cantidadVueltas unaCarrera)
 
-velocidadMaxima :: Carrera -> Int
+velocidadMaxima :: Carrera -> Float
 velocidadMaxima unaCarrera = maximum.map(velocidad).participantes $ (correrCarrera unaCarrera)
